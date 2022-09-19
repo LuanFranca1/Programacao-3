286d4156f7e061f174fd666e9785514e3d80b8c8
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-09-19 15:51:47 -0300

    Create Triangulo.java

---
+++ Atividade 2/Triangulo.java
@@ -0,0 +1,12 @@
+public class Triangulo extends Caracteristicas{
+
+    public Triangulo(){
+        
+    }
+
+    public void calcularTriangulo(Caracteristicas base,Caracteristicas altura){ 
+        double area;
+        area = base.getBase() + altura.getAltura();
+    }
+    
+}
