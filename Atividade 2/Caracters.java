011ad4f4b7bdcdfa3d3db2268002f776ad4afadb
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-09-19 15:51:28 -0300

    Create Caracters.java

---
+++ Atividade 2/Caracters.java
@@ -0,0 +1,25 @@
+public abstract class Caracteristicas {
+
+    protected double base;
+    protected double altura;
+    
+    public Caracteristicas(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
